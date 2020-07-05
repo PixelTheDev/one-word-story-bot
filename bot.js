8c800164fad6e7f247b9068a978006790b08e917
Joamonca <51541252+Joamonca@users.noreply.github.com>
2020-07-05 23:52:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,12 @@ client.on('message', message => {
     // and not get into a spam loop (we call that "botception").
     if(message.author.bot) return;
 
+    splittedmessage = message.content.split(" ");
+    numberofwords = splittedmessage.lenght;
+    if(numberofwords > 1){
+      return message.channel.send("One word per message!");
+    }
+
     // Otherwise ignore any message that does not start with the prefix, 
     // which is set above
     if(message.content.indexOf(prefix) !== 0)
@@ -76,11 +82,7 @@ client.on('message', message => {
 		
 		return message.channel.send(returnStr);
 	}
-splittedmessage = message.content.split(" ");
-    numberofwords = splittedmessage.lenght;
-    if(numberofwords > 1){
-       message.channel.send("One word per message!");
-    }   
+
 });
 
 // log the bot in
