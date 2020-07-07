e40ad5de02fceff75681bddf92a5b1103e0bc312
Joamonca <joamonca@gmail.com>
2020-07-07 16:45:11 +0200

    .

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ let channel = null;
 // from Discord _after_ ready is emitted
 client.on('ready', () => {
     //client.user.setActivity("Reading beautiful words | ./start to start!"); // set game upon login
-    client.user.setActivity("Test DB");
+    client.user.setActivity("Test");
     console.log('ready to hear your story!');
 });
 
@@ -48,7 +48,7 @@ client.on('message', message => {
 		// if listening is true, add new words to your story
 		if(listening === true && channel === message.channel)
 		{
-		  var author = message.author;
+		  var author = message.author.id;
 		  var author2;
 		  var a = message.content;
 		  var b = a.split(" ");
@@ -65,7 +65,7 @@ client.on('message', message => {
 			
 		}
 		else return;
-		  author2 = message.author;
+		  author2 = message.author.id;
 	}
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -82,15 +82,6 @@ client.on('message', message => {
 		channel = message.channel;
 		returnStr = "";
 		return message.channel.send("Now listening! Type command `;end` to stop listening.\nRemember to end your sentences, close your quotes, write only one word at a time, and have fun!");
-		///////////////
-		sql.get(`SELECT * FROM userData WHERE userId = ${messages.author.id}`).then(row => { //the row is the user"s data
-		  if (!row) { //if the user is not in the database
-		    sql.run("INSERT INTO userData (userId, money) VALUES (?, ?)", [`${guildId}`, message.author.id, 0]); //let"s just insert them
-		    message.channel.send("Registered.")
-		  } else { //if the user is in the database
-		    sql.run(`UPDATE userData SET money = ${row.money + 100} WHERE guild = ${msg.guild.id}`)
-		  }
-		});
 	}
 
 	if (command === "end")
