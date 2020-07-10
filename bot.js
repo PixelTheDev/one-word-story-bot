31d3ec73896c3a697d64ca852d5e4320f4ddcaf2
Joamonca <joamonca@gmail.com>
2020-07-10 13:43:35 +0200

    f

--- bot.js
+++ bot.js
@@ -75,7 +75,9 @@ client.on('message', message => {
       /*else if (listening === true && channel != message.channel)
       	return message.channel.send("Already listening on another channel!");*/
     } else if (c[1]) {
-      const channelarg = client.channels.get(c[1], channelName);
+      /*const channelarg = client.channels.get(c[1], channelName);*/
+      const channelarg = client.channels.cache.get(c[1]);
+      channel.send('<content>');
       listening = true;
       channel = channelarg;
       returnStr = "";
